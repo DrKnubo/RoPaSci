9b1a08d3542dd3de581eeffdbe3b07d170fd6c86
DrKnubo <s.schumacher-konstanz@gmx.de>
2023-03-03 11:58:16 +0100

    wandelt ersten buchstaben in gross

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,47 @@
+import java.util.*
+
+fun main (args: Array<String>){
+
+    val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserchoice(options)
+    printResult(userChoice, gameChoice)
+
+}
+
+fun getGameChoice(opts: Array<String>) = opts[(Math.random()*opts.size).toInt()]
+
+fun getUserchoice(opts: Array<String>) : String {
+    var  isValidChoice = false
+    var userChoice = ""
+    //Loopuntil userChoice is valid
+    while(!isValidChoice) {
+        //Ask user for choice
+        println("Please enter one of the following:")
+        for (item in opts) print(" $item")
+        println(".")
+        //Read user input
+        var userInput = readln()
+        userInput = userInput.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        //Validate users input
+        if(userInput != null && userInput in opts) {
+            isValidChoice=true
+            userChoice = userInput
+        }
+        //if the choice is invalid inform the user
+        if(!isValidChoice) println("Enter a valid choice.")
+
+    }
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String){
+    val result: String
+    //Figure out the result
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") || (userChoice == "Paper" && gameChoice=="Rock")
+        || (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You loose"
+    //print result
+    println("You chose $userChoice. I choose $gameChoice. $result")
+}
